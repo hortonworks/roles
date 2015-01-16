6f3f605cfc2abf610412e1e66665312a88cfb6f3
Scott Brimhall <sbrimhall@hortonworks.com>
2015-01-16 09:41:06 -0800

    Add role for Reaktor

---
+++ manifests/reaktor.pp
@@ -0,0 +1,29 @@
+# == Class: roles::reaktor
+#
+# Role class for Reaktor
+# https://github.com/pzim/reaktor
+#
+# === Parameters
+#
+# This is a parameter-less class as it is a role class
+#
+# === Authors
+#
+# Scott Brimhall <sbrimhall@hortonworks.com>
+#
+# === Copyright
+#
+# Copyright 2015, Hortonworks, LLC
+#
+class roles::zabbix::server {
+
+  include ::profiles::common::users
+  include ::profiles::common::sudo
+  include ::profiles::common::packages
+  include ::profiles::puppet::agent
+  include ::profiles::datadog::agent
+  include ::profiles::redis
+  include ::profiles::ruby
+  include ::profiles::git
+
+}
